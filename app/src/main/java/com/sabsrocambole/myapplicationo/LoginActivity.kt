8c01b5cdd6e924e3f8ccd66f75b962353bc727e0
sabrinadevasconcelosfernandes <sabrinadvf@ibm.com>
2022-03-03 11:49:16 -0300

    Initial commit

---
+++ app/src/main/java/com/sabsrocambole/myapplicationo/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.sabsrocambole.myapplicationo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.sabsrocambole.myapplicationo3.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun loginCreateUserBtnClicked(view: View){
+
+    }
+    fun loginLoginBtnClicked(view: View){
+
+    }
+}
